97a6f5a3e9052bf6a0168fce0bfbda2a486d665e
JianGallegoMejia <89174929+JianGallegoMejia@users.noreply.github.com>
2021-10-26 21:15:41 -0500

    Update User.java

--- javaapplication22/User.java
+++ javaapplication22/User.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication22;
 
-/**
- *
- * @author Jian_Gallego
- */
+
 public class User {
+    
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
     
 }
