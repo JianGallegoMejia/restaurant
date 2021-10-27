e68bff01446e2c7af3f515ea5ac3e80871bbe6f7
JianGallegoMejia <89174929+JianGallegoMejia@users.noreply.github.com>
2021-10-26 21:14:42 -0500

    Update Restaurant.java

--- javaapplication22/Restaurant.java
+++ javaapplication22/Restaurant.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication22;
 
-/**
- *
- * @author Jian_Gallego
- */
 public class Restaurant {
+    
+    private String name;
+    private String password;
+    private String location;
+
+    public Restaurant(String name, String password, String location) {
+        this.name = name;
+        this.password = password;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
     
 }
